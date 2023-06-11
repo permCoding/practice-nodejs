f64445ec1cf6561fb0ad2ba41905a6e5351fa8c1
Андрей Беляков <ttxiom@gmail.com>
2023-06-11 16:17:30 +0500

    up

---
+++ 2023/4_express_json/app.js
@@ -0,0 +1,14 @@
+const express = require('express'); // так подключаем внешнюю библиотеку
+const routers = require('./routers.js'); // так подключаем методы из своей библиотеки
+
+const app = express(); // объект приложения
+app.set('view engine', 'hbs'); // шаблонизатор html-страницы
+app.use(express.static('public')); // файлы этой папки будут видны браузеру
+app.use(express.urlencoded({extended:true})); // чтобы парсить параметры http-строки запроса
+app.use(express.json()); // чтобы видеть тело страницы как json-объект и брать значения объектов DOM`а
+
+app.post('/feed', routers.feed_post); // со страницы отзыва сделать запись в БД
+app.get('/feed', routers.feed_get); // перейти на страницу отзыва
+app.get('/', routers.index_get); // перейти на главную, этот путь ставить последним
+
+app.listen(3000, () => console.log(`http://localhost:${3000}/`)); // цикл прослушки событий
---
+++ 2023/4_express_json/routers.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+
+const index_get = (req, res) => {
+    let feeds = require('./private/feeds.json'); // получить массив объектов из файла
+    res.render('index.hbs', { feeds });
+}
+
+const feed_get = (req, res) => {
+    res.render('feed.hbs', { }); // render view
+}
+
+const feed_post = (req, res) => {
+    let feeds = require('./private/feeds.json');
+    let last_id = feeds[feeds.length-1].id; // последний id в массиве
+    let new_record = { // формируем объект нового отзыва
+        'id': last_id+1,
+        'name': req.body.nameUser, // находим по имени тега в шаблоне
+        'feed': req.body.feedUser // находим по имени тега в шаблоне
+    }
+    feeds.push(new_record); // добавить в конец массива
+    let json_in_str = JSON.stringify(feeds, null, 4); // перевести в строку
+    fs.writeFileSync("./private/feeds.json", json_in_str, 'utf-8'); // записать в файл
+    res.redirect('/'); // после записи редирект на главную страницу
+}
+
+module.exports = {
+    index_get,
+    feed_get,
+    feed_post
+}
